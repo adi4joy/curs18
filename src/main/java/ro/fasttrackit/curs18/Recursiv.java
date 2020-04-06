5608f193a8546e040769be8cfe2846a8e893b24b
Adi Strete <adi4joy@yahoo.com>
2020-04-06 19:04:20 +0300

    empty test result: RED

---
+++ src/main/java/ro/fasttrackit/curs18/Recursiv.java
@@ -0,0 +1,12 @@
+package ro.fasttrackit.curs18;
+
+public class Recursiv {
+    public int length(String word) {
+        if (word == null) throw new IllegalArgumentException("word cannot be null");
+        return 1;
+    }
+
+    //conditia de iesire
+
+    //clauza de reintrare
+}
---
+++ src/test/java/ro/fasttrackit/curs18/RecursivTest.java
@@ -0,0 +1,29 @@
+package ro.fasttrackit.curs18;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class RecursivTest {
+    Recursiv recursiv;
+
+    @BeforeEach
+    void setup() {
+        recursiv = new Recursiv();
+    }
+
+    @Test
+    @DisplayName("WHEN null is received THEN throw IllegalArgumentException")
+    void ifNullThenException() {
+        assertThrows(IllegalArgumentException.class, () -> recursiv.length(null));
+    }
+
+    @Test
+    @DisplayName("WHEN empty string is received THEN 0 is returned")
+    void emptyString() {
+        assertThat(recursiv.length("")).isEqualTo(0);
+    }
+}
