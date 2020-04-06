b3bfdbb6d614c4ab5ddf4bd64a9969168fc89fdb
Adi Strete <adi4joy@yahoo.com>
2020-04-06 19:06:57 +0300

    empty test result: GREEN

--- src/main/java/ro/fasttrackit/curs18/Recursiv.java
+++ src/main/java/ro/fasttrackit/curs18/Recursiv.java
@@ -3,7 +3,7 @@ package ro.fasttrackit.curs18;
 public class Recursiv {
     public int length(String word) {
         if (word == null) throw new IllegalArgumentException("word cannot be null");
-        return 1;
+        return 0;
     }
 
     //conditia de iesire
